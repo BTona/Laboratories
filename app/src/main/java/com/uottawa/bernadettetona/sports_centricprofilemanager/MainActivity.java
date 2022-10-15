b0f0fd571242415eb6f2ee9f2e7a22f78282a926
BTona <btona066@uottawa.ca>
2022-10-14 20:25:37 -0400

    Lab4

---
+++ app/src/main/java/com/uottawa/bernadettetona/sports_centricprofilemanager/MainActivity.java
@@ -0,0 +1,75 @@
+package com.uottawa.bernadettetona.sports_centricprofilemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void OnOpenInGoogleMaps (View view) {
+
+        EditText teamAddress = (EditText) findViewById(R.id.teamAddressField);
+        // Create a Uri from an intent string. Use the result to create an Intent.
+        Uri gmmIntentUri = Uri.parse("http://maps.google.co.in/maps?q="+teamAddress.getText());
+        // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        // Make the Intent explicit by setting the Google Maps package
+        mapIntent.setPackage("com.google.android.apps.maps");
+        // Attempt to start an activity that can handle the Intent
+        startActivity(mapIntent);
+    }
+
+    public void OnSetAvatarButton(View view) {
+        //Application Context and Activity
+        Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+        startActivityForResult(intent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_CANCELED) return;
+
+//Getting the Avatar Image we show to our users
+        ImageView avatarImage = (ImageView) findViewById(R.id.logoImage);
+
+//Figuring out the correct image
+        String drawableName = "leomesi";
+        switch (data.getIntExtra("imageID", R.id.imageView10)) {
+            case R.id.imageView10:
+                drawableName = "leomesi";
+                break;
+            case R.id.imageView11:
+                drawableName = "lew";
+                break;
+            case R.id.imageView12:
+                drawableName = "Dalves";
+                break;
+            case R.id.imageView20:
+                drawableName = "mbappe";
+                break;
+            case R.id.imageView21:
+                drawableName = "memphis";
+                break;
+            case R.id.imageView22:
+                drawableName = "ronaldo";
+                break;
+            default:
+                drawableName = "leomesi";
+                break;
+        }
+        int resID = getResources().getIdentifier(drawableName,"drawable", getPackageName());
+        avatarImage.setImageResource(resID);
+    }
+    }
